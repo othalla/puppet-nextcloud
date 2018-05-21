9ee1b424500ed95f090ac4b4bd6b4e000918349b
othalla <othalla.lf@gmail.com>
2018-05-21 16:07:08 +0200

    [Enhance] switch to php-fpm

--- manifests/php.pp
+++ manifests/php.pp
@@ -1,35 +1,33 @@
 class nextcloud::php {
 
-  class { '::php::globals':
-    php_version => '7.1',
-    config_root => '/etc/php/7.1',
+  class { 'phpfpm':
+    process_max  => 20,
+    package_name => 'php7.0-fpm',
+    poold_purge => true,
   }
-  -> class { '::php':
-    ensure       => latest,
-    manage_repos => false,
-    fpm          => true,
-    dev          => true,
-    composer     => true,
-    pear         => true,
-    phpunit      => false,
-    extensions   => {
-      mbstring  => {},
-      xmlrpc    => {},
-      soap      => {},
-      apcu      => {},
-      smbclient => {},
-      ldap      => {},
-      redis     => {},
-      gd        => {},
-      xml       => {},
-      intl      => {},
-      json      => {},
-      imagick   => {},
-      mysql     => {},
-      cli       => {},
-      mcrypt    => {},
-      zip       => {},
-      curl      => {},
-    },
+  -> phpfpm::pool { 'nextcloud':
+    listen => '/run/php/php7.0-fpm.sock',
+  }
+  $php_extensions = [
+    'php7.0-mbstring',
+    'php7.0-xmlrpc',
+    'php7.0-soap',
+    'php7.0-apcu',
+    'php7.0-smbclient',
+    'php7.0-ldap',
+    'php7.0-redis',
+    'php7.0-gd',
+    'php7.0-xml',
+    'php7.0-intl',
+    'php7.0-json',
+    'php7.0-imagick',
+    'php7.0-mysql',
+    'php7.0-cli',
+    'php7.0-mcrypt',
+    'php7.0-curl',
+    'php7.0-zip'
+  ]
+  package { $php_extensions:
+    ensure => present,
   }
 }
