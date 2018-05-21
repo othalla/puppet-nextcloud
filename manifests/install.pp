da53a6d9210f17d625fb95dd59a06ec0b0442410
othalla <othalla.lf@gmail.com>
2018-05-21 18:44:58 +0200

    [Fix] nextcloud tar install perms

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,17 @@
 class nextcloud::install {
 
-  archive { '/var/www/html/nextcloud-13.0.2.tar.bz2':
+  file { '/var/www/html/nextcloud':
+    ensure => directory,
+    owner  => 'www-data',
+    group  => 'www-data',
+  }
+  -> archive { '/var/www/html/nextcloud-13.0.2.tar.bz2':
     ensure       => present,
+    path         => '/tmp/nextcloud-13.0.2.tar.bz2',
     extract      => true,
     extract_path => '/var/www/html',
     source       => 'https://download.nextcloud.com/server/releases/nextcloud-13.0.2.tar.bz2',
-    creates      => '/var/www/html/nextcloud',
+    creates      => '/var/www/html/nextcloud/index.php',
     cleanup      => true,
     user         => 'www-data',
     group        => 'www-data',
