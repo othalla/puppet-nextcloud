caa34eaaa6673b53f02333a0f4cd65270c7b6df0
othalla <othalla.lf@gmail.com>
2018-12-02 20:55:23 +0100

    Add tests for default nextcloud class parameters

--- spec/classes/nextcloud_spec.rb
+++ spec/classes/nextcloud_spec.rb
@@ -7,20 +7,19 @@ describe 'nextcloud' do
         facts
       end
 
-      describe 'without ssl' do
+      describe 'with default' do
         let(:facts) do
           facts.merge(
             root_home: '/root',
           )
         end
-        let :params do
-          {
-            ssl: false,
-          }
-        end
 
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_class('nextcloud::database') }
+        it { is_expected.to contain_class('nextcloud::php') }
+        it { is_expected.to contain_class('nextcloud::install') }
+        it { is_expected.to contain_class('nextcloud::webserver') }
+        it { is_expected.to contain_class('nextcloud::config') }
       end
     end
   end
